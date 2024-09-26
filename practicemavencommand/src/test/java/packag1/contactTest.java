fa7024d604cd6c8203a80ce190b34380c39d4170
HP <HP@DESKTOP-H5NHACQ.mshome.net>
2024-09-26 12:41:01 +0530

    firstpush

---
+++ practicemavencommand/src/test/java/packag1/contactTest.java
@@ -0,0 +1,24 @@
+package packag1;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	@Test
+	public void createcontactTest() {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);		
+		System.out.println("execute create contact test");
+	}
+	
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute modify contact tets");
+	}
+
+}
---
+++ practicemavencommand/src/test/java/packag2/orgTest.java
@@ -0,0 +1,16 @@
+package packag2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("execute create org test");
+	}
+
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute modify org tets");
+	}
+
+}
